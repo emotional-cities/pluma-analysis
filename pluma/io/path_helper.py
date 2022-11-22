8a3bb6e6f048be1da4909f6089b5b4e1add1fbff
bruno-f-cruz <bfpcruz@gmail.com>
2022-11-22 14:47:24 +0000

    Initial sketch of Path class to handle s3fs remote

---
+++ pluma/io/path_helper.py
@@ -0,0 +1,76 @@
+import os
+
+from enum import Enum
+
+from s3fs.core import S3FileSystem
+
+
+class RemoteType (Enum):
+    NONE = ''
+    AWS = 'AWS'
+    WIN = 'WIN'
+    UNIX = 'UNIX'
+
+
+class Path():
+    def __init__(self,
+                 root: str = '',
+                 remotetype: RemoteType = RemoteType.WIN) -> None:
+
+        self._s3fs = S3FileSystem()
+        self._remote = RemoteType.NONE
+        self._root = self.root = root
+
+    # Instance properties
+    @property
+    def root(self):
+        return self._root
+
+    @root.setter
+    def root(self, value: str):
+        self._remote_type_conversion(value)
+        self._root = value
+
+    @property
+    def remote(self):
+        return self._remote
+
+    @remote.setter
+    def remote(self, value: RemoteType):
+        self._remote = value
+
+    # Class methods
+    def join(self, path: str, force_str: bool = False) -> str:
+        if force_str is True:
+            return os.path.join(self.root, path)
+        else:
+            return self._dynamic_path_constructor(
+                os.path.join(self.root, path)
+                )
+
+    def format(self, force_str: bool = False) -> str:
+        if force_str is True:
+            return self.root
+        else:
+            return self._dynamic_path_constructor(
+                self.root
+                )
+
+    # Helper private methods
+    def _remote_type_conversion(self, new_root: str):
+        self.remote = self._parse_remote_type(new_root)
+
+    @staticmethod
+    def _parse_remote_type(new_root: str) -> RemoteType:
+        if r's3://' in new_root:
+            return RemoteType.AWS
+        else:
+            return RemoteType.WIN
+
+    def _dynamic_path_constructor(self, path: str):
+        if self._remote == RemoteType.WIN:
+            return path
+        elif self._remote == RemoteType.AWS:
+            return self._s3fs.open(path)
+        else:
+            return path

ac671ed68641627f88c20162469e9e6ac4fccefa
bruno-f-cruz <bfpcruz@gmail.com>
2022-11-23 17:15:56 +0000

    updated acceleromter IO to be s3f compliant

--- pluma/io/accelerometer.py
+++ pluma/io/accelerometer.py
@@ -3,7 +3,9 @@ import warnings
 
 import pandas as pd
 
+from typing import Union
 from pluma.io.harp import _HARP_T0
+from pluma.io.path_helper import ComplexPath, ensure_complexpath
 
 _accelerometer_header = [
     'Orientation.X', 'Orientation.Y', 'Orientation.Z',
@@ -19,7 +21,7 @@ _accelerometer_header = [
 
 def load_accelerometer(
         filename: str = 'Accelerometer.csv',
-        root: str = '') -> pd.DataFrame:
+        root: Union[str, ComplexPath] = '') -> pd.DataFrame:
 
     """Loads the raw acceleromter data from file to a pandas DataFrame.
 
@@ -30,17 +32,26 @@ def load_accelerometer(
     Returns:
         pd.DataFrame: Dataframe with descriptive data indexed by time (Seconds)
     """
-    path = os.path.join(root, filename)
+    root = ensure_complexpath(root)
     try:
-        acc_df = pd.read_csv(
-            path,
-            header=None,
-            names=_accelerometer_header)
+        if isinstance(root, ComplexPath):
+            if root.iss3f():
+                acc_df = pd.read_csv(root.join(filename).format(),
+                                     header=None,
+                                     names=_accelerometer_header)
+            else:
+                acc_df = pd.read_csv(root.join_to_str(filename),
+                                     header=None,
+                                     names=_accelerometer_header)
+        else:
+            acc_df = pd.read_csv(root.join_to_str(filename),
+                                 header=None,
+                                 names=_accelerometer_header)
     except FileNotFoundError:
-        warnings.warn(f'Accelerometer stream file {path} could not be found.')
+        warnings.warn(f'Accelerometer stream file {root.join_to_str(filename)} could not be found.')
         return
     except FileExistsError:
-        warnings.warn(f'Accelerometer stream file {path} could not be found.')
+        warnings.warn(f'Accelerometer stream file {root.join_to_str(filename)} could not be found.')
         return
     acc_df['Seconds'] = _HARP_T0 + pd.to_timedelta(acc_df['Seconds'].values, 's')
     acc_df['SoftwareTimestamp'] = \

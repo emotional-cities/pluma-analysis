b4889d895f9fc18a7988e63508e28e2eb4a09e30
bruno-f-cruz <bfpcruz@gmail.com>
2022-11-23 09:47:15 +0000

    Ensure the automatic conversion from input str add qol shortcut methods

--- pluma/io/path_helper.py
+++ pluma/io/path_helper.py
@@ -1,6 +1,7 @@
 import os
 
 from enum import Enum
+from typing import Union
 
 from s3fs.core import S3FileSystem
 
@@ -12,7 +13,7 @@ class RemoteType (Enum):
     UNIX = 'UNIX'
 
 
-class Path():
+class ComplexPath():
     def __init__(self,
                  root: str = '',
                  remotetype: RemoteType = RemoteType.WIN) -> None:
@@ -38,6 +39,12 @@ class Path():
     @remote.setter
     def remote(self, value: RemoteType):
         self._remote = value
+
+    def iswin(self):
+        return self.remote == RemoteType.WIN
+
+    def iss3f(self):
+        return self.remote == RemoteType.AWS
 
     # Class methods
     def join(self, path: str, force_str: bool = False) -> str:
@@ -74,3 +81,18 @@ class Path():
             return self._s3fs.open(path)
         else:
             return path
+
+    def __str__(self) -> str:
+        return f'@({self.remote.value}) --> {self.root}'
+
+    def __repr__(self) -> str:
+        return f'@({self.remote.value}) --> {self.root}'
+
+# ----- Helper functions -----
+
+
+def ensure_complexpath(root: Union[str, ComplexPath]) -> ComplexPath:
+    if isinstance(root, str):
+        return ComplexPath(root=root)
+    else:
+        return root

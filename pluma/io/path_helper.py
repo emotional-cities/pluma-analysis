c2e4bb000d60e0070e760647478967048da02b3d
bruno-f-cruz <bfpcruz@gmail.com>
2022-11-25 10:05:08 +0000

    refactor Complexpath to simplify opening files from AWS

--- pluma/io/path_helper.py
+++ pluma/io/path_helper.py
@@ -3,8 +3,6 @@ import os
 from enum import Enum
 from typing import Union
 
-from s3fs.core import S3FileSystem
-
 
 class RemoteType (Enum):
     NONE = ''
@@ -14,34 +12,38 @@ class RemoteType (Enum):
 
 
 class ComplexPath():
-    s3fs = S3FileSystem()
+    s3fs = None  # Stores the s3fs object that is instantiated when needed
 
     def __init__(self,
-                 root: str = '',
-                 remotetype: RemoteType = RemoteType.WIN) -> None:
+                 path: str = '') -> None:
 
         self._remote = RemoteType.NONE
-        self._root = root
-        self.root = self._root
+        self._path = path
+        self.path = self._path
+
+    def open(self, *args, **kwargs):
+        if self.iss3f():
+            return self.s3fs.open(self.path, *args, **kwargs)
+        else:
+            return open(self.path, *args, **kwargs)
+
+    def exists_s3fs(self):
+        return not(self.s3fs is None)
 
     # Instance properties
     @property
-    def root(self):
-        return self._root
+    def path(self):
+        return self._path
 
-    @root.setter
-    def root(self, value: str):
+    @path.setter
+    def path(self, value: str):
         self._remote_type_conversion(value)
         value = self._format_os_path(value)
-        self._root = value
+        self._path = value
 
     @property
     def remote(self):
         return self._remote
-
-    @remote.setter
-    def remote(self, value: RemoteType):
-        self._remote = value
 
     def iswin(self):
         return self.remote == RemoteType.WIN
@@ -49,27 +51,16 @@ class ComplexPath():
     def iss3f(self):
         return self.remote == RemoteType.AWS
 
-    def str(self):
-        return self.root
-
-    # Class methods
-    def join_to_str(self, path: str) -> str:
-        return self._format_os_path(os.path.join(self.root, path))
-
-    def join(self, path: str):
-        return ComplexPath(self.join_to_str(path))
-
-    def append(self, path: str) -> None:
-        self.root = self.join(path)
-
-    def format(self, force_str: bool = False) -> str:
-        if force_str is True:
-            return self.root
+    def join(self, value: Union[str, list]):
+        if isinstance(value, str):
+            self.path = os.path.join(self.path, value)
+        elif isinstance(value, list):
+            _path = self.path
+            for ss in value:
+                _path = os.path.join(_path, ss)
+            self.path = _path
         else:
-            return self._dynamic_path_constructor(
-                self.root
-                )
-
+            raise ValueError("")
     # Helper private methods
     def _format_os_path(self, path):
         if self.remote == RemoteType.WIN:
@@ -82,7 +73,10 @@ class ComplexPath():
             return path.replace("\\", "/")
 
     def _remote_type_conversion(self, new_root: str):
-        self.remote = self._parse_remote_type(new_root)
+        self._remote = self._parse_remote_type(new_root)
+        if (self._remote == RemoteType.AWS) and (self.s3fs is None):
+            from s3fs.core import S3FileSystem
+            self.s3fs = S3FileSystem()
 
     @staticmethod
     def _parse_remote_type(new_root: str) -> RemoteType:
@@ -91,36 +85,20 @@ class ComplexPath():
         else:
             return RemoteType.WIN
 
-    def _dynamic_path_constructor(self, path: str):
-        if self._remote == RemoteType.WIN:
-            return path
-        elif self._remote == RemoteType.AWS:
-            return self.s3fs.open(path)
-        else:
-            return path
-
     def __str__(self) -> str:
-        return f'@({self.remote.value}) --> {self.root}'
+        return f'@({self.remote.value}) --> {self.path}'
 
     def __repr__(self) -> str:
-        return f'@({self.remote.value}) --> {self.root}'
+        return f'@({self.remote.value}) --> {self.path}'
 
 # ----- Helper functions -----
 
 
-def ensure_complexpath(root: Union[str, ComplexPath]) -> ComplexPath:
-    if isinstance(root, str):
-        return ComplexPath(root=root)
-    elif isinstance(root, ComplexPath):
-        return root
+def ensure_complexpath(in_path: Union[str, ComplexPath]) -> ComplexPath:
+    if isinstance(in_path, str):
+        return ComplexPath(path=in_path)
+    elif isinstance(in_path, ComplexPath):
+        return ComplexPath(in_path.path)
     else:
-        raise TypeError("Must be of str or ComplexPath type")
-
-
-def ensure_strpath(root: Union[str, ComplexPath]) -> str:
-    if isinstance(root, str):
-        return root
-    elif isinstance(root, ComplexPath):
-        return ComplexPath(root)
-    else:
-        raise TypeError("Must be of str or ComplexPath type")
+        raise TypeError(
+            f"Must be of str or ComplexPath type. Got {type(in_path)}")

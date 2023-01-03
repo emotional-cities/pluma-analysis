327f877aaa54c526573423d530868efe7bd60913
bruno-f-cruz <bfpcruz@gmail.com>
2023-01-03 20:19:40 +0000

    Fix typo

--- pluma/schema/outdoor.py
+++ pluma/schema/outdoor.py
@@ -73,13 +73,13 @@ def build_schema(root: Union[str, ComplexPath],
     streams.TK.PTC.AirTemp =      	  			  HarpStream(245, device='TK', streamlabel='PTC.AirTemp', root=root, autoload=autoload)
 
     # ATMOS streams
-    streams.Atmos.NorthWind =               	  HarpStream(246, device='Atoms', streamlabel='NorthWind', root=root, autoload=autoload)
-    streams.Atmos.EastWind =              		  HarpStream(247, device='Atoms', streamlabel='EastWind', root=root, autoload=autoload)
-    streams.Atmos.GustWind =                      HarpStream(248, device='Atoms', streamlabel='GustWind', root=root, autoload=autoload)
-    streams.Atmos.AirTemperature =			  	  HarpStream(249, device='Atoms', streamlabel='AirTemperature', root=root, autoload=autoload)
-    streams.Atmos.XOrientation =                  HarpStream(250, device='Atoms', streamlabel='XOrientation', root=root, autoload=autoload)
-    streams.Atmos.YOrientation =                  HarpStream(251, device='Atoms', streamlabel='YOrientation', root=root, autoload=autoload)
-    streams.Atmos.NullValue =                     HarpStream(252, device='Atoms', streamlabel='NullValue', root=root, autoload=autoload)
+    streams.Atmos.NorthWind =               	  HarpStream(246, device='Atmos', streamlabel='NorthWind', root=root, autoload=autoload)
+    streams.Atmos.EastWind =              		  HarpStream(247, device='Atmos', streamlabel='EastWind', root=root, autoload=autoload)
+    streams.Atmos.GustWind =                      HarpStream(248, device='Atmos', streamlabel='GustWind', root=root, autoload=autoload)
+    streams.Atmos.AirTemperature =			  	  HarpStream(249, device='Atmos', streamlabel='AirTemperature', root=root, autoload=autoload)
+    streams.Atmos.XOrientation =                  HarpStream(250, device='Atmos', streamlabel='XOrientation', root=root, autoload=autoload)
+    streams.Atmos.YOrientation =                  HarpStream(251, device='Atmos', streamlabel='YOrientation', root=root, autoload=autoload)
+    streams.Atmos.NullValue =                     HarpStream(252, device='Atmos', streamlabel='NullValue', root=root, autoload=autoload)
 
     # Accelerometer streams
     streams.Accelerometer =                       AccelerometerStream(device='Accelerometer', streamlabel='Accelerometer', root=root, autoload=autoload)

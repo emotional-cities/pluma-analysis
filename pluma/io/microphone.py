962d313d6d038b3476f4fe125289e848b4776c8c
bruno-f-cruz <bfpcruz@gmail.com>
2023-03-25 12:22:31 +0000

    Fix bug in microphone loader and implement channel reshaping

--- pluma/io/microphone.py
+++ pluma/io/microphone.py
@@ -8,7 +8,7 @@ from pluma.io.path_helper import ComplexPath, ensure_complexpath
 def load_microphone(filename: str = 'Microphone.bin',
                     channels: int = 2,
                     root: Union[str, ComplexPath] = '',
-                    dtype=np.uint16
+                    dtype=np.int16
                     ) -> np.array:
     """Loads microphone waveform data from a file into a numpy array.
 
@@ -17,7 +17,7 @@ def load_microphone(filename: str = 'Microphone.bin',
         channels (int, optional): Number of expected audio input channels. Defaults to 2.
         root (Union[str, ComplexPath], optional): Root path\
             where filename is expected to be found. Defaults to ''.
-        dtype (_type_, optional): Data type of the binary file. Defaults to np.uint16.
+        dtype (_type_, optional): Data type of the binary file. Defaults to np.int16.
 
     Returns:
         np.array: Array with raw waveform data from the microphone stream.
@@ -29,6 +29,7 @@ def load_microphone(filename: str = 'Microphone.bin',
     try:
         with path.open('rb') as stream:
             micdata = np.frombuffer(stream.read(), dtype=dtype)
+            micdata = micdata.reshape((-1, channels))
     except FileNotFoundError:
         warnings.warn(f'Microphone stream file\
             {path} could not be found.')

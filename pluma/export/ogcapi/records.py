59626e961c131ac9bea4c4f87fe2e4d10d0a70ce
glopesdev <glopesdev@users.noreply.github.com>
2023-08-25 13:17:18 +0100

    Add template based OGC records JSON renderer

--- pluma/export/ogcapi/records.py
+++ pluma/export/ogcapi/records.py
@@ -1,3 +1,4 @@
+import jinja2
 import isodate
 import pandas as pd
 from pathlib import Path
@@ -5,6 +6,14 @@ from geopandas import GeoDataFrame
 from datetime import datetime
 
 from pluma.schema import Dataset
+
+_env = jinja2.Environment(
+    loader=jinja2.PackageLoader("pluma"),
+    autoescape=jinja2.select_autoescape)
+_template = _env.get_template("metadata_template.j2")
+
+def _format_timestamp(time_utc: datetime) -> str:
+    return f"{time_utc.isoformat()}Z"
 
 class MetadataRecord:
     def __init__(self, dataset: Dataset, gdf: GeoDataFrame) -> None:
@@ -19,4 +28,20 @@ class MetadataRecord:
         self.crs = gdf.crs
 
     def to_json(self) -> str:
-        return ""
+        return _template.render(
+            id=self.id,
+            start_date=_format_timestamp(self.start_date),
+            end_date=_format_timestamp(self.end_date),
+            created_timestamp=_format_timestamp(self.created_timestamp),
+            updated_timestamp=_format_timestamp(self.updated_timestamp),
+            resolution=self.resolution,
+            coordinates=[[
+                [self.bounds[0], self.bounds[1]],
+                [self.bounds[0], self.bounds[3]],
+                [self.bounds[2], self.bounds[3]],
+                [self.bounds[2], self.bounds[1]],
+                [self.bounds[0], self.bounds[1]]
+            ]],
+            crs=self.crs.to_string(),
+            keywords=[],
+            themes=[])

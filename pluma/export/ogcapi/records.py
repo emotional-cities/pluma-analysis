a3b48c8c94331d80458335f260d056053894dbb8
glopesdev <glopesdev@users.noreply.github.com>
2023-08-27 15:07:53 +0100

    Add data classes for record properties

--- pluma/export/ogcapi/records.py
+++ pluma/export/ogcapi/records.py
@@ -6,6 +6,7 @@ from geopandas import GeoDataFrame
 from datetime import datetime
 
 from pluma.schema import Dataset
+from dataclasses import dataclass, field, fields
 
 _env = jinja2.Environment(
     loader=jinja2.PackageLoader("pluma"),
@@ -15,8 +16,29 @@ _template = _env.get_template("metadata_template.j2")
 def _format_timestamp(time_utc: datetime) -> str:
     return f"{time_utc.isoformat()}Z"
 
-class MetadataRecord:
-    def __init__(self, dataset: Dataset, gdf: GeoDataFrame) -> None:
+@dataclass
+class Contact:
+    name: str
+    institution: str
+    email: str
+
+@dataclass
+class Theme:
+    scheme_url: str
+    concepts: list[str] = field(default_factory=lambda: [])
+
+@dataclass
+class RecordProperties:
+    title: str
+    description: str
+    license: str
+    tool: str
+    keywords: list[str] = field(default_factory=lambda: [])
+    contacts: list[Contact] = field(default_factory=lambda: [])
+    themes: list[Theme] = field(default_factory=lambda: [])
+
+class DatasetRecord:
+    def __init__(self, dataset: Dataset, gdf: GeoDataFrame, properties: RecordProperties) -> None:
         root_path = Path(dataset.rootfolder.path)
         self.id = f"{root_path.name}.geojson"
         self.start_date = gdf.index[0]
@@ -26,6 +48,7 @@ class MetadataRecord:
         self.resolution = isodate.duration_isoformat(pd.Timedelta(gdf.index.freq))
         self.bounds = gdf.total_bounds
         self.crs = gdf.crs
+        self.properties = properties
 
     def to_json(self) -> str:
         return _template.render(
@@ -43,5 +66,5 @@ class MetadataRecord:
                 [self.bounds[0], self.bounds[1]]
             ]],
             crs=self.crs.to_string(),
-            keywords=[],
-            themes=[])
+            **(dict((field.name, getattr(self.properties, field.name))
+                    for field in fields(self.properties))))

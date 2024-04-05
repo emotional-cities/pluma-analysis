93c9df1d137fc27ad4a10ff527d53dc0babcaec9
glopesdev <glopesdev@users.noreply.github.com>
2024-04-05 03:56:39 +0100

    Add support for historical locations

--- pluma/export/ogcapi/sta.py
+++ pluma/export/ogcapi/sta.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from typing import Any, Union
 from dotmap import DotMap
+from geojson import Point
 import frost_sta_client as fsc
 from requests import HTTPError
 from pluma.export.ogcapi.records import RecordProperties
 from pluma.schema import Dataset
 from pluma.stream import Stream
-from itertools import groupby
 from dataclasses import dataclass, field
 
 class SensorThingsProperties:
@@ -29,13 +29,31 @@ class SensorThingsDataset:
     thing: fsc.Thing
     properties: SensorThingsProperties
     datastreams: list[fsc.Datastream] = field(default_factory=lambda: [])
+    historical_locations: list[fsc.HistoricalLocation] = field(default_factory=lambda: [])
 
 def convert_dataset_to_sta(dataset: Dataset, properties: SensorThingsProperties) -> SensorThingsDataset:
     root_path = Path(dataset.rootfolder.path)
     thing = fsc.Thing(
         name = f"{root_path.name.lower()}",
-        description = dataset.datasetlabel
+        description = dataset.datasetlabel,
+        properties = { "withLocation": True }
     )
+
+    historical_locations = []
+    for row in dataset.georeference.spacetime.itertuples():
+        time = row.Index
+        point = Point((row.Longitude, row.Latitude))
+        location = fsc.Location(
+            name = "location",
+            description = "GPS coordinates of the wearable backpack",
+            encoding_type = "application/geo+json",
+            location = point
+        )
+        historical_locations.append(fsc.HistoricalLocation(
+            locations = [location],
+            time = time.strftime(f"%Y-%m-%dT%H:%M:%S.%fZ"),
+            thing = thing
+        ))
 
     datastreams = []
     for stream in recursive_iterate_streams(dataset.streams):
@@ -52,18 +70,20 @@ def convert_dataset_to_sta(dataset: Dataset, properties: SensorThingsProperties)
             sensor = sensor,
             thing = thing
         ))
-    return SensorThingsDataset(thing, properties, datastreams)
+    return SensorThingsDataset(thing, properties, datastreams, historical_locations)
 
 def create_sta_properties(properties: SensorThingsProperties, service: fsc.SensorThingsService):
     for sensor in properties.sensors.values():
         try_create(sensor, service)
     for observed_property in properties.observed_properties.values():
-        try_create(sensor, observed_property)
+        try_create(observed_property, service)
 
 def create_sta_entities(sta_dataset: SensorThingsDataset, service: fsc.SensorThingsService):
     service.create(sta_dataset.thing)
     for datastream in sta_dataset.datastreams:
         service.create(datastream)
+    for historical_location in sta_dataset.historical_locations:
+        service.create(historical_location)
 
 def recursive_iterate_streams(streams):
     for stream in streams.values():

676d1fb412867a454bf80a6df8165fa767b5fe55
glopesdev <glopesdev@users.noreply.github.com>
2024-05-06 07:25:00 +0100

    Resample geospatial data using geopandas

--- pluma/preprocessing/resampling.py
+++ pluma/preprocessing/resampling.py
@@ -1,20 +1,21 @@
 import datetime
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 
 from scipy.stats import circmean
 
 
-def resample_temporospatial(input_data: pd.DataFrame,
+def resample_temporospatial(data: pd.DataFrame,
                             georeference: pd.DataFrame,
-                            sampling_dt:
-                                datetime.timedelta
-                                = datetime.timedelta(seconds=2))\
-                                    -> pd.DataFrame:
+                            sampling_dt: datetime.timedelta
+                                = datetime.timedelta(seconds=2),
+                            aggregate_func=None)\
+                                    -> gpd.GeoDataFrame:
     """Temporally resamples a temporally indexed data stream and aligns it to a spatial reference.
 
     Args:
-        input_data (pd.DataFrame): DataFrame with data index by time
+        data (pd.DataFrame): DataFrame with data index by time
         georeference (pd.DataFrame): a geoference, usually the output of Streams.ubxStream.UbxStream.parseposition, or equivalent.
         sampling_dt (datetime.timedelta, optional): _description_. Defaults to datetime.timedelta(seconds = 2).
 
@@ -22,38 +23,40 @@ def resample_temporospatial(input_data: pd.DataFrame,
         ValueError: Raises an error if the input DataFrame is empty.
 
     Returns:
-        pd.DataFrame: Returns a resampled DataFrame.
+        gpd.GeoDataFrame: Returns a resampled GeoDataFrame.
     """
-    if input_data.empty:
+    if data.empty:
         raise ValueError("Input dataframe is empty.")
 
     resampled = georeference.spacetime.loc\
         [:, "Latitude":"Elevation"].resample(
             sampling_dt, origin='start').mean()
-    resampled['Data'] = np.NAN
-    for i in np.arange(len(resampled)-1):
-        resampled['Data'].iloc[i] = (input_data[
-            (input_data.index >= resampled.index[i]) &
-            (input_data.index < resampled.index[i+1])]).mean()
-    resampled['Data'].iloc[i+1] = (
-        input_data[input_data.index >= resampled.index[i+1]]).mean()
-    return resampled
+    resampled_data_index = resampled.index[resampled.index.searchsorted(data.index)]
+    resampled_data = data.groupby(resampled_data_index)
+
+    if (aggregate_func is None):
+        resampled_data = resampled_data.mean()
+    else:
+        resampled_data = resampled_data.apply(aggregate_func)
+        
+    resampled_data = resampled_data.reindex(resampled.index)
+    return _create_geodataframe(resampled_data, resampled)
 
 
-def resample_temporospatial_circ(input_data,
+def resample_temporospatial_circ(data,
                                  georeference,
                                  sampling_dt=datetime.timedelta(seconds=2)):
-    resampled = georeference.spacetime.loc[:, "Latitude":"Elevation"].\
-        resample(sampling_dt, origin='start').mean()
-    resampled['Data'] = np.NAN
-    for i in np.arange(len(resampled)-1):
-        resampled['Data'].iloc[i] = circular_mean((input_data[
-            (input_data.index >= resampled.index[i]) &
-            (input_data.index < resampled.index[i+1])]))
-    resampled['Data'].iloc[i+1] = circular_mean(
-        input_data[input_data.index >= resampled.index[i+1]]
-        )
-    return resampled
+    return resample_temporospatial(data, georeference, sampling_dt, circular_mean)
+
+
+def _create_geodataframe(data, spacetime):
+    geometry = gpd.points_from_xy(
+        x=spacetime['Longitude'],
+        y=spacetime['Latitude'],
+        z=spacetime['Elevation'])
+    gdf = gpd.GeoDataFrame(data, geometry=geometry)
+    gdf.crs = 'epsg:4326'
+    return gdf
 
 
 def circular_mean(x):
